b6236f6a385c14b0ab8ad214b841fb45f9e47794
Alistair Willis <alistairrwillis@gmail.com>
2017-04-01 09:22:59 +0900

    update

--- dist/prod-server.js
+++ dist/prod-server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static("app"));
+app.use(express.static("dist"));
 
 app.listen(port, (err) => {
     if (err) {
