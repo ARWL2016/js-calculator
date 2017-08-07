a7667609ced3829325d8e60d4a2342066ca7c478
Alistair Willis <alistairrwillis@gmail.com>
2017-08-07 13:32:39 +0900

    rebuild prod for bugfix

--- dist/prod-server.js
+++ dist/prod-server.js
@@ -4,7 +4,7 @@ var compression = require('compression');
 
 var app = express();
 app.use(compression());
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
